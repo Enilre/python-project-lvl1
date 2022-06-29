cae19a9f45f19df2ff6fcde8d171c8fb3f4981fa
re aleksandr <resashaa@gmail.com>
2022-06-29 12:53:31 +0300

    add linter, add badge Maintainability in README.md, add setup.cfg

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,4 +1,8 @@
+"""Script for greeting in Brain games."""
 import prompt
+
+
 def welcome_user():
+    """Ask a player name."""
     name = prompt.string('May I have your name? ')
-    print("Hello, " + name + "!")
+    print('Hello, {0}!'.format(name))  # print('Hello, ' + name + '!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,14 @@
+"""Script for Brain Games."""
 #!/usr/bin/env python
 from brain_games.cli import welcome_user
 
+
 def main():
+    """Welcoming user and asking for name."""
     print('Welcome to the Brain Games!')
     welcome_user()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
     welcome_user()
